864db4f0048929234723483e591ddb9a94324c22
pnandez <paquiherca@gmail.com>
2022-05-04 15:54:35 +0100

    Tests passed works on codewars

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
-console.log('Hello world!')
+export function orderWeight(weightsInNumbers: string): string {
+  const numbers = weightsInNumbers.match(/\d+/g)?.map((str) => String(str)) || [];
+  console.log(numbers);
+  const orderedNumbersByWeights = numbers?.sort((a, b) => {
+    if (calculateWeight(a) > calculateWeight(b)) return 1;
+    if (calculateWeight(a) < calculateWeight(b)) return -1;
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+  })!;
+  return orderedNumbersByWeights.join(" ");
+}
+
+function calculateWeight(numberValue: string) {
+  const digits = numberValue.split("");
+  let sum = 0;
+  digits.forEach((digit) => {
+    sum += parseInt(digit, 10);
+  });
+  return sum;
+}
+
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,6 +1,15 @@
-describe('Basic tests', () => {
-    it('Always green', () => {
-      expect(true).toBe(true)
-    });
+/* eslint-disable object-curly-spacing */
+
+import { orderWeight } from "../src/main";
+
+describe("Fixed Tests", () => {
+  it("orderWeight", () => {
+    expect(orderWeight("103 123 4444 99 2000")).toBe("2000 103 123 4444 99");
+    expect(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123")).toBe("11 11 2000 10003 22 123 1234000 44444444 9999");
   });
-  
+
+  it("accepts leading whitespaces or separation by more than one whitespace", () => {
+    expect(orderWeight("103   123 4444   99 2000")).toBe("2000 103 123 4444 99");
+    expect(orderWeight("      2000 10003 1234000 44444444 9999 11 11 22 123")).toBe("11 11 2000 10003 22 123 1234000 44444444 9999");
+  });
+});
